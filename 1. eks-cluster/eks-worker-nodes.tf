2f34b2fc488dba14c36a9a3885b3b5b4f2f3e658
kaonmir <thswpvm@cau.ac.kr>
2022-03-12 10:10:39 +0900

    remove redundant resources

--- 1. eks-cluster/eks-worker-nodes.tf
+++ 1. eks-cluster/eks-worker-nodes.tf
@@ -78,8 +78,8 @@ resource "aws_eks_node_group" "nodegroup_admin" {
 
   scaling_config {
     desired_size = 1
-    min_size     = 2
-    max_size     = 1
+    min_size     = 1
+    max_size     = 2
   }
 
   depends_on = [
