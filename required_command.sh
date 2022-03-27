1ed04c23c8e8984065eae9952f619b32158c4469
kaonmir <thswpvm@cau.ac.kr>
2022-03-27 10:21:57 +0900

    change requried command bash shell

--- required_command.sh
+++ required_command.sh
@@ -14,4 +14,4 @@ curl -sL https://istio.io/downloadIstioctl | sh -
 cp $HOME/.istioctl/bin/istioctl /usr/local/bin/istioctl
 rm -rf $HOME/.istioctl
 
-echo "In advanced to run this commands, you must login to AWS CLI with your credential."
+echo "In advanced to run this commands, you must login to AWS CLI with your credential ."
