711d6eddf01670f34264b6295f78cf3d856061ac
kaonmir <thswpvm@cau.ac.kr>
2022-05-04 12:38:10 +0900

    change amount of worker node

--- 1. eks-cluster/eks-worker-nodes.tf
+++ 1. eks-cluster/eks-worker-nodes.tf
@@ -79,7 +79,7 @@ resource "aws_eks_node_group" "nodegroup_admin" {
   scaling_config {
     desired_size = 1
     min_size     = 1
-    max_size     = 4
+    max_size     = 4 
   }
 
   depends_on = [
