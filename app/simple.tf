19429adcf5218312e557aea506135cbcb19dcbf3
kaonmir <thswpvm@cau.ac.kr>
2022-06-10 09:20:34 +0900

    submodule simple-architecture is modified

--- app/simple.tf
+++ app/simple.tf
@@ -2,8 +2,11 @@ terraform {
   required_version = ">= 0.12"
 }
 
+# https://registry.terraform.io/modules/kaonmir/simple-architecture/aws/0.0.0
+# https://github.com/kaonmir/terraform-aws-simple-architecture/releases/tag/v0.0.0
 module "nemo_simple" {
-  source       = "../0. simple-aws-architecutre"
+  source       = "kaonmir/simple-architecture/aws"
+  version      = "0.1.0"
   project_name = "nemo"
   aws_region   = "ap-northeast-2"
 }
