37bbeff12f6eb0738f2eb5561450e33b86705c0a
kaonmir <thswpvm@cau.ac.kr>
2022-06-10 14:33:58 +0900

    keep up-to-date the submodules

--- app/simple.tf
+++ app/simple.tf
@@ -2,8 +2,14 @@
 # https://registry.terraform.io/modules/kaonmir/simple-architecture/aws
 module "simple" {
   # count  = 0 # skip
-  source = "kaonmir/simple-architecture/aws"
-  # version      = "0.2.0"
+  source       = "kaonmir/simple-architecture/aws"
+  version      = "0.5.0"
   project_name = "nemo"
   aws_region   = "ap-northeast-2"
+
+  image = {
+    registry   = "value"
+    repository = "value"
+    tag        = "value"
+  }
 }
