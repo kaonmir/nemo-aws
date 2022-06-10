cb0adb30bfb2426528c3437f887062f56f555bfb
kaonmir <thswpvm@cau.ac.kr>
2022-06-10 10:15:31 +0900

    make cluster autoscale to submodule

--- app/simple.tf
+++ app/simple.tf
@@ -6,7 +6,7 @@ terraform {
 # https://github.com/kaonmir/terraform-aws-simple-architecture/releases/tag/v0.0.0
 module "nemo_simple" {
   source       = "kaonmir/simple-architecture/aws"
-  version      = "0.1.0"
+  version      = "0.2.0"
   project_name = "nemo"
   aws_region   = "ap-northeast-2"
 }
