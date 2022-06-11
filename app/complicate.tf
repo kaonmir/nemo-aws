0473297cdfa3ad15ec2bc9c9844f323bb53e5001
kaonmir <thswpvm@cau.ac.kr>
2022-06-11 16:55:59 +0900

    update eks-cluster submodule

--- app/complicate.tf
+++ app/complicate.tf
@@ -6,41 +6,21 @@ locals {
 module "eks-cluster" {
   # count   = 0 # skip
   source  = "kaonmir/nemo-eks-cluster/aws"
-  version = "0.7.1"
+  version = "0.9.0"
 
-  aws_region   = local.region
-  cluster_name = local.cluster_name
+  aws_region       = local.region
+  cluster_name     = local.cluster_name
+  make_kube_config = true
+
+
 }
 
 module "eks-cluster-autoscale" {
   count = 0 # skip
-  depends_on = [module.eks-cluster.cluster,
-  ]
+  # depends_on = [module.eks-cluster.cluster]
   source  = "kaonmir/nemo-eks-cluster-autoscale/aws"
   version = "0.3.0"
 
   aws_region   = local.region
   cluster_name = local.cluster_name
 }
-
-# ---
-
-module "eks-cluster-2" {
-  # count   = 0 # skip
-  source  = "kaonmir/nemo-eks-cluster/aws"
-  version = "0.7.1"
-
-  aws_region   = local.region
-  cluster_name = "semo"
-}
-
-module "eks-cluster-autoscale-2" {
-  count      = 0 # skip
-  source     = "kaonmir/nemo-eks-cluster-autoscale/aws"
-  depends_on = [module.eks-cluster-2.cluster]
-  version    = "0.3.0"
-
-  aws_region   = local.region
-  cluster_name = "semo"
-}
-
